5df849a4b419de1810440d21d3583a50123fd30a
Laukik Rupne <laukikrupne11@gmail.com>
2021-06-15 23:19:40 +0530

    different styles of using selectors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
   templateUrl: './app.component.html',
   //styleUrls: ['./app.component.css']
   styles: [`
