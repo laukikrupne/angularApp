ad6efd7b7f7c4c45dc059ba2e2d6c69afb7a64d5
Laukik Rupne <laukikrupne11@gmail.com>
2021-06-18 21:37:50 +0530

    String interpolation in action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   // selector: '[app-servers]',
-  // selector: '.app-servers',
+  // selector: '.app-servers', 
   templateUrl: './app.component.html',
   //styleUrls: ['./app.component.css']
   styles: [`
