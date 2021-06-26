89d35c172e4a049d448570b5b67b2307e8a81b42
Laukik Rupne <laukikrupne11@gmail.com>
2021-06-26 09:34:43 +0530

    Directives with *ngIf and #

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppComponent } from './app.component';
 import {ServerComponent} from "./server/server.component";
 import { ServersComponent } from './servers/servers.component';
 import {FormsModule} from "@angular/forms";
-import { AssignmentComponent } from './assignment/assignment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent,
-    AssignmentComponent
+    ServersComponent
   ],
   imports: [
     BrowserModule,
